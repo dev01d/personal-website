ff58f71e8fa8b9d9c81cd8983fb9d6c621b7c4a2
Jason Behnke <jbehnke259@gmail.com>
2017-12-24 09:10:52 -0800

    A little shake and beautify
    Got rid of some old code, added some CDNs for css, and added cleaned up syntax/formatting.

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,18 @@
 // preloader
-$(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+$(window).load(function () {
+  $('.preloader').fadeOut(1000); // set duration in brackets    
 });
 /* HTML document is loaded. DOM is ready. 
 -------------------------------------------*/
-$(function(){
+$(function () {
   // ------- WOW ANIMATED ------ //
-  wow = new WOW(
-  {
+  wow = new WOW({
     mobile: false
   });
   wow.init();
   // ------- JQUERY PARALLAX ---- //
   function initParallax() {
-   // -- $('#home').parallax("100%", 0.1); --//
+    // -- $('#home').parallax("100%", 0.1); --//
     $('#gallery').parallax("100%", 0.3);
     $('#menu').parallax("100%", 0.2);
     $('#team').parallax("100%", 0.3);
@@ -21,12 +20,11 @@ $(function(){
   }
   initParallax();
   // HIDE MOBILE MENU AFTER CLIKING ON A LINK
-  $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
+  $('.navbar-collapse a').click(function () {
+    $(".navbar-collapse").collapse('hide');
+  });
   // NIVO LIGHTBOX
   $('#gallery a').nivoLightbox({
-        effect: 'fadeScale',
-    });
+    effect: 'fadeScale',
+  });
 });
-
