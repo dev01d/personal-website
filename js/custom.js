51e1d4176ea3dff59a55a20b564e859cba69d34d
Jason Behnke <contact@jasonbehnke.net>
2018-06-01 16:06:15 -0700

    Better JS scoping

--- js/custom.js
+++ js/custom.js
@@ -3,18 +3,12 @@ $(window).load(function() {
   $('.preloader').fadeOut(500) // set duration
 })
 // Wow animations
-
 const wow = new WOW({
   // not on mobile
   mobile: false
 })
 wow.init()
-
 // HIDE MOBILE MENU AFTER CLIKING ON A LINK
 $('.navbar-collapse a').click(function() {
   $('.navbar-collapse').collapse('hide')
 })
-// Lightbox
-;(function($) {
-  $('.swipebox').swipebox()
-})(jQuery)
