640f9897c647237709053508d4cec40f25a5a46d
Jason Behnke <contact@jasonbehnke.net>
2018-06-01 14:50:41 -0700

    rework lightbox, trim external files & bump versions

--- js/custom.js
+++ js/custom.js
@@ -2,21 +2,19 @@
 $(window).load(function() {
   $('.preloader').fadeOut(500) // set duration
 })
-/* HTML document is loaded. DOM is ready. 
--------------------------------------------*/
-$(function() {
-  // ------- WOW ANIMATED ------ //
-  var wow = new WOW({
-    // not on mobile
-    mobile: false
-  })
-  wow.init()
-  // HIDE MOBILE MENU AFTER CLIKING ON A LINK
-  $('.navbar-collapse a').click(function() {
-    $('.navbar-collapse').collapse('hide')
-  })
-  // NIVO LIGHTBOX
-  $('#gallery a').nivoLightbox({
-    effect: 'fadeScale'
-  })
+// Wow animations
+
+const wow = new WOW({
+  // not on mobile
+  mobile: false
 })
+wow.init()
+
+// HIDE MOBILE MENU AFTER CLIKING ON A LINK
+$('.navbar-collapse a').click(function() {
+  $('.navbar-collapse').collapse('hide')
+})
+// Lightbox
+;(function($) {
+  $('.swipebox').swipebox()
+})(jQuery)
