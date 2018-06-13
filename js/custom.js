d9d7e0608a6e92dad1e995bd1fa0c35d42a3a221
Jason Behnke <contact@jasonbehnke.net>
2018-06-12 21:52:11 -0700

    Add SH Sentry

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,7 @@
+// Sentry init
+Raven.config(
+  'https://8fc9bb60a6d64dd68e99edde1b84582d@sentry.jasonbehnke.net/2'
+).install()
 // preloader
 $(window).load(function() {
   $('.preloader').fadeOut(500) // set duration
