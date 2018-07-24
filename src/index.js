f09a3d7a293eac27853afc5faa01f8d48436fb7b
Jason Behnke <contact@jasonbehnke.net>
2018-07-24 12:47:11 -0700

    remove place holders and restructure

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom'
 
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <Main />
-      </div>
-    )
+    return <Main />
   }
 }
 
@@ -15,63 +11,50 @@ class Main extends React.Component {
   render() {
     return (
       <div>
-        <section>
-          <div className="container">
-            <div className="row">
-              <ul className="social-icon">
-                <li>
-                  <a
-                    href="https://github.com/dev01d"
-                    className="fab fa-github"
-                    alt="github"
-                  />
-                </li>
-                <li>
-                  <a
-                    href="mailto:contact@jasonbehnke.net?subject=Inquiry"
-                    className="far fa-envelope"
-                    alt="email"
-                  />
-                </li>
-                <li>
-                  <a
-                    href="https://instagram.com/jason.behnke"
-                    className="fab fa-instagram"
-                    alt="link to instagram"
-                  />
-                </li>
-                <li>
-                  <a
-                    href="https://500px.com/jasonbehnke"
-                    className="fab fa-500px"
-                    alt="500px"
-                  />
-                </li>
-                <li>
-                  <a
-                    href="https://vimeo.com/jasonbehnke"
-                    className="fab fa-vimeo-v"
-                    alt="vimeo"
-                  />
-                </li>
-              </ul>
-              <div className="row">
-                <ul className="connect">
-                  <p>Let's connect</p>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </section>
         <section id="home">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 col-sm-12">
-                <img src="https://res.cloudinary.com/dev01d/q_auto,f_auto/full/main.png" />
-              </div>
+          <div className="row">
+            <div>
+              <img src="https://res.cloudinary.com/dev01d/q_auto,f_auto/full/main.png" />
             </div>
           </div>
         </section>
+        <ul className="social-icon">
+          <li>
+            <a
+              href="https://github.com/dev01d"
+              className="fab fa-github"
+              alt="github"
+            />
+          </li>
+          <li>
+            <a
+              href="mailto:contact@jasonbehnke.net?subject=Inquiry"
+              className="far fa-envelope"
+              alt="email"
+            />
+          </li>
+          <li>
+            <a
+              href="https://instagram.com/jason.behnke"
+              className="fab fa-instagram"
+              alt="link to instagram"
+            />
+          </li>
+          <li>
+            <a
+              href="https://500px.com/jasonbehnke"
+              className="fab fa-500px"
+              alt="500px"
+            />
+          </li>
+          <li>
+            <a
+              href="https://vimeo.com/jasonbehnke"
+              className="fab fa-vimeo-v"
+              alt="vimeo"
+            />
+          </li>
+        </ul>
       </div>
     )
   }
