a8c91116ffe7ee47b963584e27f1aa3e5a5a2f41
Jason Behnke <contact@jasonbehnke.io>
2018-12-19 01:35:15 -0800

    Add Sentry

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import * as Sentry from '@sentry/browser'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import './styles/index'
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: process.env.SENTRY_KEY
+  })
+}
 
 ReactDOM.render(<App />, document.getElementById('app'))
