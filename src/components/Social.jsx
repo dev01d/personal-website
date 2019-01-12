73b161af251bf2da6d5409b2449a9aa0a92e017a
Jason Behnke <contact@jasonbehnke.io>
2019-01-11 20:30:47 -0800

    Add date

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,45 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Social = () => (
-  <div>
-    <ul className="social-icon">
-      <li>
-        <a
-          href="https://github.com/dev01d"
-          className="fab fa-github"
-          alt="github"
-        />
-      </li>
-      <li>
-        <a
-          href="mailto:contact@jasonbehnke.io?subject=Inquiry"
-          className="far fa-envelope"
-          alt="email"
-        />
-      </li>
-      <li>
-        <a
-          href="https://instagram.com/jason.behnke"
-          className="fab fa-instagram"
-          alt="link to instagram"
-        />
-      </li>
-      <li>
-        <a
-          href="https://500px.com/jasonbehnke"
-          className="fab fa-500px"
-          alt="500px"
-        />
-      </li>
-      <li>
-        <a
-          href="https://vimeo.com/jasonbehnke"
-          className="fab fa-vimeo-v"
-          alt="vimeo"
-        />
-      </li>
-    </ul>
-  </div>
-)
-
-export default Social
+export default class Social extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { date: new Date().getFullYear() }
+  }
+  render() {
+    return (
+      <div className="social-icon">
+        <ul>
+          <li>
+            <a
+              href="https://github.com/dev01d"
+              className="fab fa-github"
+              alt="github"
+            />
+          </li>
+          <li>
+            <a
+              href="mailto:contact@jasonbehnke.io?subject=Inquiry"
+              className="far fa-envelope"
+              alt="email"
+            />
+          </li>
+          <li>
+            <a
+              href="https://instagram.com/jason.behnke"
+              className="fab fa-instagram"
+              alt="link to instagram"
+            />
+          </li>
+          <li>
+            <a
+              href="https://500px.com/jasonbehnke"
+              className="fab fa-500px"
+              alt="500px"
+            />
+          </li>
+          <li>
+            <a
+              href="https://vimeo.com/jasonbehnke"
+              className="fab fa-vimeo-v"
+              alt="vimeo"
+            />
+          </li>
+        </ul>
+        <p>&copy; 2015 - {this.state.date}</p>
+      </div>
+    )
+  }
+}
