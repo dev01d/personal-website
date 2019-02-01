1c0655f8aa480e95e9a5d5e199e1b0b43a889264
Jason Behnke <contact@jasonbehnke.io>
2019-01-31 20:24:56 -0800

    That var DSN tho

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './styles/index.css'
 
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
-    dsn: 'https://def76a2b2849416aa29bbcb82c41ed3e@sentry.dev01d.com/2'
+    dsn: process.env.SENTRY_DSN
   })
 }
 
