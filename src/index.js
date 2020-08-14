99b80ada075af144ae1b11b29b5940ae5ceb247d
Jason Behnke <contact@jasonbehnke.io>
2020-08-13 20:00:42 -0700

    Sentry :bug: :mag:

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactGA from 'react-ga'
 import ReactDOM from 'react-dom'
 import AppRouter from './router/AppRouter'
-import Rollbar from "rollbar"
+import * as Sentry from '@sentry/browser'
 import 'normalize.css'
 import './styles/App.css'
 import './styles/index.css'
@@ -10,14 +10,12 @@ import './styles/index.css'
 if (process.env.NODE_ENV === 'production') {
   ReactGA.initialize(process.env.GA_CODE, {
     gaOptions: {
-      siteSpeedSampleRate: 100
-    }
+      siteSpeedSampleRate: 100,
+    },
   })
-  Rollbar.init({
-    accessToken: process.env.ROLLBAR_TOKEN,
-    captureUncaught: true,
-    captureUnhandledRejections: true
-  });
+  Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  })
 }
 
 ReactDOM.render(<AppRouter id="page-wrap" />, document.getElementById('app'))
