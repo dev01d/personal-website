fcf5802319140a80d7c95d4dea7053963d227f2c
Jason Behnke <contact@jasonbehnke.io>
2021-10-30 07:02:16 -0700

    Buildx config
    Basically notes for ci

---
+++ docker_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Building docker containers for arm64 and x64"
+sleep 5
+# Build the static docker image
+docker buildx build --push --platform=linux/amd64,linux/arm64 . -f docker/Dockerfile-static -t dev01d/personal-site:static
+# Build the node docker image
+docker buildx build --push --platform=linux/amd64,linux/arm64 . -f docker/Dockerfile-node -t dev01d/personal-site:node
