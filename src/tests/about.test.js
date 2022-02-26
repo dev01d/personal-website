051e96e37893120749406010c3cc334e0e4b5ed8
Jason Behnke <jason@jasonbehnke.io>
2022-02-25 19:22:53 -0800

    Move tests off root

--- src/tests/about.test.js
+++ src/tests/about.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import AboutPage from '../pages/about'
+import AboutPage from '../../pages/about'
 
 describe('AboutPage', () => {
   it('Should render json content', () => {
