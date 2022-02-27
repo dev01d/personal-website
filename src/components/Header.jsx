70d60399f14ab03ebb5cf80c8143506dac3d6e69
Jason Behnke <jason@jasonbehnke.io>
2022-02-27 14:03:13 -0800

    Update icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,11 +26,19 @@ export default function Header() {
           sizes="120x120"
           href="./assets/apple-touch-icon.png"
         />
+
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
           href="./assets/favicon-32x32.png"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="./assets/favicon-16x16.png"
         />
 
         <link
