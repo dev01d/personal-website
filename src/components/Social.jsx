daf536da3c13ab832ffe4a9f58d4437542c94a23
Jason Behnke <jason@jasonbehnke.io>
2022-07-26 04:29:59 -0700

    aria

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -14,7 +14,11 @@ const Social = () => {
       <base rel="noopener noreferrer" target="_blank" />
       <ul>
         <li>
-          <a href="https://github.com/dev01d" alt="link to github.com">
+          <a
+            href="https://github.com/dev01d"
+            alt="link to github.com"
+            aria-label="link to github.com"
+          >
             <FontAwesomeIcon icon={faGithub} inverse fixedWidth />
           </a>
         </li>
@@ -22,17 +26,26 @@ const Social = () => {
           <a
             href="mailt&#111;&#58;&#99;o&#110;tact&#64;jaso&#110;behnk&#101;&#46;io"
             alt="send me an email"
+            aria-label="send me an email"
           >
             <FontAwesomeIcon icon={faEnvelope} inverse fixedWidth />
           </a>
         </li>
         <li>
-          <a href="https://instagram.com/jason.behnke" alt="link to instagram">
+          <a
+            href="https://instagram.com/jason.behnke"
+            alt="link to instagram"
+            aria-label="link to instagram"
+          >
             <FontAwesomeIcon icon={faInstagram} inverse fixedWidth />
           </a>
         </li>
         <li>
-          <a href="https://dev.to/dev01d" alt="link to Dev.to">
+          <a
+            href="https://dev.to/dev01d"
+            alt="link to Dev.to"
+            aria-label="link to Dev.t"
+          >
             <FontAwesomeIcon icon={faDev} inverse fixedWidth />
           </a>
         </li>
