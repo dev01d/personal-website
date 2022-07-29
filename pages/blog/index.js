379cd2bee8d595236db5d996dbbd445e38f7f7ef
Jason Behnke <jason@jasonbehnke.io>
2022-07-29 02:40:10 -0700

    grid css break on number of posts

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,23 +2,29 @@ import PostCard from '../../src/components/PostCard'
 import getPosts from '../../src/utils/getPosts'
 
 export default function Blog({ posts }) {
+  const layout = posts.length % 2 && posts.length < 4 ? true : false
+  let postItems = posts.map((post) => (
+    <PostCard
+      key={post.slug}
+      image={post.data.cover_image}
+      title={post.data.title}
+      date={post.data.date}
+      description={post.data.description}
+      slug={post.slug}
+    />
+  ))
   return (
-    <div className="mt-24 pb-4 grid">
-      <h1 className="pb-12 font-bold text-5xl dark:text-white underline">
+    <div className="mt-24 pb-4 grid mx-auto max-w-sm md:max-w-2xl lg:max-w-2xl">
+      <h1 className="pb-12 font-bold text-5xl dark:text-white underline min-w-fit ml-2">
         Latest Posts
       </h1>
-      <div className="grid grid-flow-row grid-cols-2 m:grid-cols-0 gap-4 ml-4">
-        {posts.map((post) => (
-          <PostCard
-            key={post.slug}
-            image={post.data.cover_image}
-            title={post.data.title}
-            date={post.data.date}
-            description={post.data.description}
-            slug={post.slug}
-          />
-        ))}
-      </div>
+      {layout ? (
+        <div className="grid grid-flow-row grid-cols-0 gap-4">{postItems}</div>
+      ) : (
+        <div className="grid grid-flow-row grid-cols-0 md:grid-cols-2 gap-4">
+          {postItems}
+        </div>
+      )}
     </div>
   )
 }
