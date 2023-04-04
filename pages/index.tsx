e5471924e60b62649890ca06e784634fc19f7584
Jason Behnke <jason@jasonbehnke.io>
2023-04-03 20:51:46 -0700

    Fix static build svg url

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   var logoID = '/assets/logo.svg'
-  if (process.env.NEXT_PUBLIC_BUILD_ENV == 'static') {
-    var logoID = `c_scale,w_600/logo.svg`
-  }
   return (
     <main>
       <div className="max-w-lg mx-auto mt-0">
