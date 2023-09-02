bdfbff7752c34fd1b15ccc8025f199995dcfcfaa
Jason Behnke <jason@jasonbehnke.io>
2023-09-01 17:41:42 -0700

    extend-expect was merged into global

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ const createJestConfig = nextJest({
 })
 
 const customJestConfig = {
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
 }
