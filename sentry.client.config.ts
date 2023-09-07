e4a3cda06d7d58eaf6c1eced0db846de44cdfeaf
Jason Behnke <jason@jasonbehnke.io>
2023-09-07 14:17:16 -0700

    sampleRate

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,13 +2,6 @@ import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  integrations: [
-    new Sentry.Replay({
-      // Additional Replay configuration goes in here, for example:
-      sessionSampleRate: 1.0,
-      errorSampleRate: 1.0,
-      maskAllText: true,
-      blockAllMedia: true,
-    }),
-  ],
+  replaysSessionSampleRate: 1,
+  replaysOnErrorSampleRate: 1,
 })
