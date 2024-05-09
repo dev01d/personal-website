a739f46d695a511002393258640dce1b05744158
Jason Behnke <jason@jasonbehnke.io>
2024-05-08 22:33:15 -0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,26 +1,25 @@
 const { withSentryConfig } = require('@sentry/nextjs')
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = withSentryConfig(
-  nextConfig,
-  {
-    silent: true,
-
-    org: process.env.SENTRY_ORG,
-    project: process.env.SENTRY_PROJECT,
+// /** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
-  {
+  sentry: {
+    disableServerWebpackPlugin: true,
+    disableClientWebpackPlugin: true,
     widenClientFileUpload: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: '/monitoring',
-
-    // Hides source maps from generated client bundles
     hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
-  }
-)
+  },
+}
+
+const sentryWebpackPluginOptions = {
+  silent: true,
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+}
+
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
