a14d0505db82abd0189501ad33c8133a1bee5dd7
Jason Behnke <jason@jasonbehnke.io>
2024-05-11 18:58:12 -0700

    Fix docker pipeline

--- docker/static/next.config.js
+++ docker/static/next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
+module.exports = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
 }
-
-module.exports = nextConfig
