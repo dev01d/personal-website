f10502faf4eb8fe27d4005cdf1e12cc0b65bef33
Jason Behnke <jason@jasonbehnke.io>
2025-05-25 12:33:54 -0700

    Remove Sentry configuration files and simplify metrics workflow

--- next.config.js
+++ next.config.js
@@ -5,20 +5,7 @@ const { withSentryConfig } = require('@sentry/nextjs')
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  sentryBuildOptions: {
-    disableServerWebpackPlugin: true,
-    disableClientWebpackPlugin: true,
-    widenClientFileUpload: true,
-    hideSourceMaps: true,
-    disableLogger: true,
-  },
+  reactStrictMode: true,
 }
 
-const sentryWebpackPluginOptions = {
-  silent: true,
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-}
-
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
+module.exports = withSentryConfig(nextConfig)
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  mode: 'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
